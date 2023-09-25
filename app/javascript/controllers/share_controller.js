1d5175c956316b501b1f8d81a4ef726dd17da120
Lukasz <lukaszgoralczyk2205@gmail.com>
2023-09-25 21:12:42 +0200

    removed c logs

--- app/javascript/controllers/share_controller.js
+++ app/javascript/controllers/share_controller.js
@@ -4,20 +4,15 @@ export default class extends Controller {
     static targets = ['author', 'content', 'result']
 
     connect() {
-        console.log(this.contentTarget.textContent);
-        console.log(this.authorTarget.textContent);
-        console.log(this.data.get('urlValue'));
     }
 
     async share(e) {
         e.preventDefault()
-
-
         const shareData = {
             content: this.contentTarget.textContent,
             author: this.authorTarget.textContent,
             url: this.data.get('urlValue')
-        };
+        }
         try {
             await navigator.share(shareData);
             this.resultTarget.textContent = 'Thanks for sharing';
@@ -25,6 +20,5 @@ export default class extends Controller {
             this.resultTarget.textContent = `something went wrong ${err}`;
         }
 
-        console.log(shareData);
     }
 }
