6ce00837bda91f300f5de94869195d15aeac42b3
Lukasz <lukaszgoralczyk2205@gmail.com>
2023-10-26 18:24:27 +0200

    added mutation observer

--- app/javascript/controllers/autoscroll_controller.js
+++ app/javascript/controllers/autoscroll_controller.js
@@ -1,10 +1,21 @@
-import { Controller } from "@hotwired/stimulus"
+import {Controller} from "@hotwired/stimulus"
 
 export default class extends Controller {
     static targets = ['chat']
 
     connect() {
         this.scrollToBottom()
+        const observer = new MutationObserver(function (mutationsList, observer) {
+            console.log('xxxx')
+            for (let mutation of mutationsList) {
+                if (mutation.target.parentElement) {
+                    console.log('Zmiana wykryta!');
+                    this.scrollToBottom()
+                }
+            }
+        }.bind(this));
+        const config = {childList: true, subtree: true, attributes: true};
+        observer.observe(this.chatTarget, config);
     }
 
     scrollToBottom() {
