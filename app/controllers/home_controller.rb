98e6c74af0db4996d127ca5ccb2baaca77de212e
Lukasz <lukaszgoralczyk2205@gmail.com>
2023-11-07 19:35:11 +0100

    added lazy loading for posts on root page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,10 @@ class HomeController < ApplicationController
   def index
     @post = Post.new
     # @comment = current_user.profile.comments.new
+
+  end
+
+  def index_lazy
     @posts = Post.all.order('created_at DESC')
   end
 end
