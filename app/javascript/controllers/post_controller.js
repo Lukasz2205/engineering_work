ad0ec60556bd857c9650aeec3f69ca80937b5b8c
Lukasz <lukaszgoralczyk2205@gmail.com>
2023-11-09 21:59:11 +0100

    Added show more func

---
+++ app/javascript/controllers/post_controller.js
@@ -0,0 +1,25 @@
+import { Controller } from '@hotwired/stimulus'
+
+export default class extends Controller {
+    static targets = ['content', 'button']
+
+    connect() {
+        const content = this.contentTarget
+
+        if (content.textContent.length >= 500) {
+            console.log(content.textContent)
+            const showMoreLink = document.createElement('a')
+            const showMoreText = document.getElementById('show-more')
+            showMoreLink.setAttribute('href', '#');
+            showMoreLink.setAttribute('data-action', 'click->post#showMore');
+            showMoreLink.text = "Pokaż więcej..."
+            content.parentElement.querySelector('.show-more').appendChild(showMoreLink)
+        }
+    }
+
+    showMore(event) {
+        event.preventDefault()
+        this.contentTarget.classList.remove('post-limit')
+        this.buttonTarget.classList.add('hidden')
+    }
+}
