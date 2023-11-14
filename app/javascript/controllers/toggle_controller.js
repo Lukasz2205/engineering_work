d1f8527adf77ad1433ed1382082f9ef9c0728028
Lukasz <lukaszgoralczyk2205@gmail.com>
2023-11-14 17:57:45 +0100

    some fixes and added cookie to keep panel expanded

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -9,9 +9,11 @@ export default class extends Controller {
     this.sidebarTarget.classList.toggle('active');
 
     if(button.classList.contains('bxs-right-arrow')) {
+      document.cookie = "sidebar_active=true";
       button.classList.remove('bxs-right-arrow')
       button.classList.add('bxs-left-arrow')
     } else {
+      document.cookie = "sidebar_active=false";
       button.classList.add('bxs-right-arrow')
       button.classList.remove('bxs-left-arrow')
     }
