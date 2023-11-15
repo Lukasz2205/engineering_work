c11696535d534d72f86cb1fc6a5727fa1ae21d1b
Lukasz <lukaszgoralczyk2205@gmail.com>
2023-11-15 17:47:17 +0100

    fixed cookies ad added lazy to index

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -9,11 +9,13 @@ export default class extends Controller {
     this.sidebarTarget.classList.toggle('active');
 
     if(button.classList.contains('bxs-right-arrow')) {
-      document.cookie = "sidebar_active=true";
+      document.cookie = 'sidebar_active' + '=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+      document.cookie = "sidebar_active=true;path=/";
       button.classList.remove('bxs-right-arrow')
       button.classList.add('bxs-left-arrow')
     } else {
-      document.cookie = "sidebar_active=false";
+      document.cookie = 'sidebar_active' + '=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+      document.cookie = "sidebar_active=false;path=/";
       button.classList.add('bxs-right-arrow')
       button.classList.remove('bxs-left-arrow')
     }
