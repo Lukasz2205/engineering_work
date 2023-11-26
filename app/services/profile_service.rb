de0ee2572d41dfa1c4e62635877870a0a0e2f3f3
Lukasz <lukaszgoralczyk2205@gmail.com>
2023-11-26 21:39:26 +0100

    fix

--- app/services/profile_service.rb
+++ app/services/profile_service.rb
@@ -6,9 +6,8 @@ class ProfileService
   def get_likes
     liked_posts = @profile.likes.where(likeable_type: 'Post').pluck(:likeable_id)
     liked_comments = @profile.likes.where(likeable_type: 'Comment').pluck(:likeable_id)
-    posts = Post.where(id: liked_posts).order(' created_at DESC')
-    posts += Comment.where(id: liked_comments).order(' created_at DESC')
-    posts
+    posts = Post.where(id: liked_posts).order(created_at: :desc)
+    posts += Comment.where(id: liked_comments).order(created_at: :desc)
   end
 
   def get_comments
