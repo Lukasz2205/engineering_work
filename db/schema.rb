934611f652b11e80f3a08b1d7704df4a9cefecce
Lukasz <lukaszgoralczyk2205@gmail.com>
2023-11-26 18:09:50 +0100

    added missing active storage tables migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_09_150519) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_26_170749) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -58,13 +58,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_09_150519) do
   create_table "followings", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.uuid "follower_id"
     t.uuid "followed_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "follows", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.uuid "follower_id"
-    t.uuid "followee_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
