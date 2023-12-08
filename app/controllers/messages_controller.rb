dbf37c027a16a2545dcdc79b4b516b577cc7b181
Lukasz2205 <lukaszgoralczyk2205@gmail.co>
2023-12-08 13:00:46 +0100

    added removing info of empty chat when add message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,9 @@
 class MessagesController < ApplicationController
   def create
     @message = current_user_profile.messages.create(content: msg_params[:content], room_id: params[:room_id])
+    respond_to do |format|
+      render turbo_stream: turbo_stream.remove('info-message')
+    end
   end
 
   private
