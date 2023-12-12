b0d7d6896720e0366157d8bf68e219b6ba35dc0a
Lukasz2205 <lukaszgoralczyk2205@gmail.com>
2023-12-12 19:04:33 +0100

    added close modal when click on modal background

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -13,10 +13,4 @@ export default class extends Controller {
     this.modalTarget.style.display = 'none';
     document.body.classList.remove('modal-open');
   }
-
-  outsideClick(e) {
-    if (e.target == modal) {
-      this.modalTarget.style.display = 'none';
-    }
-  }
 }
