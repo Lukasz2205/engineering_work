03554d989d15fc9a0c88babe475accc6774ef923
Lukasz2205 <lukaszgoralczyk2205@gmail.com>
2023-12-12 18:19:19 +0100

    added modal to posts

---
+++ app/javascript/controllers/modal_controller.js
@@ -0,0 +1,22 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="modal"
+export default class extends Controller {
+  static targets = ['button', 'modal']
+
+  openModal() {
+    this.modalTarget.style.display = 'block';
+    document.body.classList.add('modal-open');
+  }
+
+  closeModal() {
+    this.modalTarget.style.display = 'none';
+    document.body.classList.remove('modal-open');
+  }
+
+  outsideClick(e) {
+    if (e.target == modal) {
+      this.modalTarget.style.display = 'none';
+    }
+  }
+}
