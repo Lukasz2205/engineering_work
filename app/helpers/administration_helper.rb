84fe6474cab318768ec897178e730f129c4373e6
Lukasz2205 <lukaszgoralczyk2205@gmail.com>
2023-12-12 15:23:59 +0100

    added seeds and update for users table with user role

--- app/helpers/administration_helper.rb
+++ app/helpers/administration_helper.rb
@@ -2,4 +2,8 @@ module AdministrationHelper
   def block_information(status)
     status == false ? 'Brak' : 'Zablokowany'
   end
+
+  def user_role(role)
+    role == 'admin' ? 'Administrator' : 'Użytkownik'
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,28 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 #
-user1 = User.create(email: 'example@example.com', password: '123456', role: 'admin', profile: Profile.create(name: 'Example user1'))
-user2 = User.create(email: 'example2@example.com', password: '123456', role: 'admin', profile: Profile.create(name: 'Example user2'))
+user1 = User.create(email:    'example@example.com',
+                    password: '123456',
+                    role:     'admin',
+                    profile:  Profile.create(name:         Faker::Name.name,
+                                              description: Faker::Quote.jack_handey))
+user2 = User.create(email:    'example2@example.com',
+                    password: '123456',
+                    role:     'admin',
+                    profile:  Profile.create(name:        Faker::Name.name,
+                                             description: Faker::Quote.jack_handey))
+20.times do
+  User.create(email:    "#{Faker::Name.first_name}@example.com",
+              password: '123456',
+              role:     'user',
+              profile:  Profile.create(name:        Faker::Name.name,
+                                       description: Faker::Quote.jack_handey))
+end
 
 profile1 = user1.profile
 profile2 = user2.profile
 
 10.times do
-  n = 0
-  profile1.posts.create(description: "Post utworzony testowo #{n += 1}")
-  profile2.posts.create(description: "Post utworzony testowo #{n += 3}")
+  profile1.posts.create(description: Faker::Lorem.sentence(word_count: rand(1..500)))
+  profile2.posts.create(description: Faker::Lorem.sentence(word_count: rand(1..500)))
 end
