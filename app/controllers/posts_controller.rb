fc442cb42f9e12a35e93231fc1238506b6c3dbc6
Lukasz2205 <lukaszgoralczyk2205@gmail.com>
2023-12-12 23:48:10 +0100

    adjustment for videos

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,7 +27,7 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
-    if @post.update(set_post)
+    if @post.update(params.require(:post).permit(:description))
       redirect_to post_path(@post), notice: 'Pomyślnie zaktualizowano post!'
     else
       redirect_to root_path, alert: 'Coś poszło nie tak'
--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -2,15 +2,37 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="modal"
 export default class extends Controller {
-  static targets = ['button', 'modal']
+  static targets = ['button', 'modal', 'imageModal']
 
   openModal() {
     this.modalTarget.style.display = 'block';
     document.body.classList.add('modal-open');
   }
 
-  closeModal() {
-    this.modalTarget.style.display = 'none';
+  openImageModal() {
+    this.imageModalTarget.style.display = 'block';
+    document.body.classList.add('modal-open');
+  }
+
+  closeImageModal() {
+    this.imageModalTarget.style.display = 'none';
+    this.disableVideo()
     document.body.classList.remove('modal-open');
   }
-}
+
+  closeModal() {
+    this.modalTarget.style.display = 'none';
+    this.disableVideo()
+    document.body.classList.remove('modal-open');
+  }
+
+  disableVideo() {
+    const videos = document.querySelectorAll('video')
+    if (videos.length > 0) {
+      videos.forEach((video) => {
+        video.pause()
+      })
+    }
+
+  }
+  }
