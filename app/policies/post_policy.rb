26aae1e2e2115f0191002bade5453642dd3335b3
Lukasz2205 <87777925+Lukasz2205@users.noreply.github.com>
2023-12-12 16:52:10 +0100

    Add_faker_gem (#33)
    * Added faker to gem file
    
    * added seeds and update for users table with user role
    
    * Adjustment layout
    
    * Added truncate and expanded seeds
    
    * seeds update
    
    * added missing class

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -7,11 +7,18 @@ class PostPolicy < ApplicationPolicy
     user_or_admin? && !blocked?
   end
   def destroy?
-    user.profile == record.profile || user.admin?
+    owner_or_admin?
+  end
+
+  def owner_admin?
+    owner_or_admin?
   end
 
   private
 
+  def owner_or_admin?
+    user.profile == record.profile || user.admin?
+  end
   def user_or_admin?
     user.user? || user.admin?
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,54 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 #
-user1 = User.create(email: 'example@example.com', password: '123456', role: 'admin', profile: Profile.create(name: 'Example user1'))
-user2 = User.create(email: 'example2@example.com', password: '123456', role: 'admin', profile: Profile.create(name: 'Example user2'))
+user1 = User.create(email:    'example@example.com',
+                    password: '123456',
+                    role:     'admin',
+                    profile:  Profile.create(name:         Faker::Name.name,
+                                              description: Faker::Quote.jack_handey))
+user2 = User.create(email:    'example2@example.com',
+                    password: '123456',
+                    role:     'admin',
+                    profile:  Profile.create(name:        Faker::Name.name,
+                                             description: Faker::Quote.jack_handey))
+20.times do
+  User.create(email:    "#{Faker::Name.first_name}@example.com",
+              password: '123456',
+              role:     'user',
+              profile:  Profile.create(name:        Faker::Name.name,
+                                       description: Faker::Quote.jack_handey))
+end
 
 profile1 = user1.profile
 profile2 = user2.profile
 
 10.times do
-  n = 0
-  profile1.posts.create(description: "Post utworzony testowo #{n += 1}")
-  profile2.posts.create(description: "Post utworzony testowo #{n += 3}")
+  profile1.posts.create(description: Faker::Lorem.sentence(word_count: rand(1..500)))
+  profile2.posts.create(description: Faker::Lorem.sentence(word_count: rand(1..500)))
 end
+
+private_room = Room.create_private_room([profile1, profile2],  "private_#{profile1.id}_#{profile2.id}")
+public_room  = profile1.rooms.create(name: 'Publiczny pokój')
+
+10.times do
+  private_room.messages.create(profile: profile1, content: Faker::Lorem.sentence(word_count: rand(1..30)))
+  private_room.messages.create(profile: profile2, content: Faker::Lorem.sentence(word_count: rand(1..30)))
+end
+
+profiles = Profile.where.not(id: profile1).take(10)
+20.times do
+  public_room.messages.create(profile: profiles[rand(0..9)], content: Faker::Lorem.sentence(word_count: rand(1..30)))
+end
+
+profiles.each do |profile|
+  profile1.followees << profile
+end
+profile2.followees << profile1
+
+post = profile1.posts.first
+post.likes.create(profile: profile2)
+notification = NotificationsService.new(profile2, post).create_like_notification
+notification.save
+post.comments.create(message: Faker::Lorem.sentence(word_count: rand(1..30)), profile: profile2)
+notification2 = NotificationsService.new(profile2, post).create_comment_notification
+notification2.save
